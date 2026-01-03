2286f471ac01f323857da2aae6ddf10c0d1e56f0
Amodu Kehinde <amodukehindee@gmail.com>
2026-01-03 01:47:30 +0100

    feat: client-side encryption
    - Implement AES-GCM encryption/decryption
    - Protect sensitive data at rest
    - Use authenticated encryption with nonce

---
+++ core/crypto.go
@@ -0,0 +1,25 @@
+package core
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+)
+
+func Encrypt(data, key []byte) ([]byte, error) {
+	block, _ := aes.NewCipher(key)
+	gcm, _ := cipher.NewGCM(block)
+
+	nonce := make([]byte, gcm.NonceSize())
+	rand.Read(nonce)
+
+	return gcm.Seal(nonce, nonce, data, nil), nil
+}
+
+func Decrypt(data, key []byte) ([]byte, error) {
+	block, _ := aes.NewCipher(key)
+	gcm, _ := cipher.NewGCM(block)
+
+	nonceSize := gcm.NonceSize()
+	return gcm.Open(nil, data[:nonceSize], data[nonceSize:], nil)
+}
